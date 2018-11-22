e2a0b2101e76b92a5c3c666a69ae815b2d77c058
Soha Mourad <Soha.Mourad@triosstudent.com>
2018-11-21 19:47:07 -0500

    first commit

---
+++ PIZZAProject/Reciept.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PIZZAProject
+{
+
+
+    /// <summary>
+    /// Interaction logic for Reciept.xaml
+    /// </summary>
+    public partial class Reciept : Window
+    {
+        public float hst;
+        public float final;
+
+
+
+        public Reciept()
+        {
+            InitializeComponent();
+            UserN1.Content = "Welcome" + "   " + MainWindow.Name;
+
+            totalb.Content = Window2.totaal ;
+
+            HstTx.Content = (Window2.totaal * (13.00f))/100;
+
+            totalAftTx.Content = (Window2.totaal * 0.13 )+ Window2.totaal ;
+
+
+
+        }
+
+        private void Neworder_Click(object sender, RoutedEventArgs e)
+        {
+            Window2 win3 = new Window2();
+            win3.Show();
+            this.Hide();
+
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+          
+
+        }
+
+        
+    }
+}
---
+++ PIZZAProject/Window2.xaml.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PIZZAProject
+{
+    /// <summary>
+    /// Interaction logic for Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        float sizeprice = 0;
+        public int pann = 2;
+        public int count;
+        public float addon=0;
+        public static float totaal;
+       
+
+
+        public Window2()
+        {
+
+            InitializeComponent();
+
+
+
+           
+
+            UserN.Content = "Welcome" + "   " + MainWindow.Name;
+
+
+
+        }
+
+        public void addons()
+        {
+            Console.WriteLine("checked");
+            count++;
+            if (count > 3)
+            {
+                int extras = count - 3;
+                addon = extras * 1;
+            }
+            else
+
+            {
+                addon = 0;
+                
+
+            }
+
+            total.Content = (sizeprice + addon + pann).ToString() + "$";
+            totaal = sizeprice + addon + pann;
+        }
+    
+        public void removeaddons()
+        {
+            Console.WriteLine("unchecked");
+            
+            Console.WriteLine("before removing " + count);
+            count--;
+            if (count > 3)
+            {
+                int extras = count - 3;
+                addon= extras * 1;
+            }
+            else
+
+            {
+                addon = 0;
+               
+
+            }
+
+            total.Content = (sizeprice + addon + pann).ToString() + "$";
+            totaal = sizeprice + addon + pann;
+        }
+
+
+        private void Medium_Checked(object sender, RoutedEventArgs e)
+        {
+            if (medium.IsChecked == true )
+
+
+            {
+                
+                sizeprice = 8;
+
+                total.Content = sizeprice.ToString() + "$";
+                totaal = sizeprice;
+            }
+           
+        }
+
+        private void Large_Checked(object sender, RoutedEventArgs e)
+        {
+            if (large.IsChecked == true)
+            {
+                sizeprice = 12;
+                total.Content = sizeprice.ToString() + "$";
+                totaal = sizeprice;
+
+            }
+           
+        }
+
+        private void Small_Checked(object sender, RoutedEventArgs e)
+        {
+            if (small.IsChecked == true)
+            {
+                sizeprice = 5;
+                total.Content = sizeprice.ToString() + "$";
+                totaal = sizeprice;
+            }
+           
+        }
+
+        private void Thin_Checked(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void Pan_Checked(object sender, RoutedEventArgs e)
+        {
+             if ( pan.IsChecked == true && thin.IsChecked==false  && thick.IsChecked==false)
+            {
+                total.Content = (sizeprice + pann).ToString() + "$";
+                totaal = sizeprice + pann;
+            }
+            else
+            {
+                pann = 0;
+            }
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            small.IsChecked = false;
+            medium.IsChecked = false;
+            large.IsChecked = false;
+            thin.IsChecked = false;
+            thick.IsChecked = false;
+            pan.IsChecked = false;
+            
+            GreenOlive.IsChecked = false;
+            BlackOlive.IsChecked = false;
+            SGPepper.IsChecked = false;
+            SRPepper.IsChecked = false;
+            mashroom.IsChecked = false;
+            RedOnion.IsChecked = false;
+            pepproni.IsChecked = false;
+            tomato.IsChecked = false;
+            hotP.IsChecked = false;
+            total.Content = " ";
+            totaal = 0;
+
+         //   MessageBox.Show(totaal.ToString());
+
+        }
+
+        private void GreenOlive_Checked(object sender, RoutedEventArgs e)
+        {
+
+
+            if (GreenOlive.IsChecked == true)
+        {
+                addons();
+        }
+            else
+            {
+                removeaddons();
+            }
+          
+        }
+
+        private void BlackOlive_Checked(object sender, RoutedEventArgs e)
+        {
+            if (BlackOlive.IsChecked == true)
+            {
+                addons();
+            }
+            else
+            {
+                removeaddons();
+            }
+        }
+
+        private void SGPepper_Checked(object sender, RoutedEventArgs e)
+        {
+            if (SGPepper.IsChecked == true)
+            {
+                addons();
+            }
+            else
+            {
+                removeaddons();
+            }
+        }
+
+        private void SRPepper_Checked(object sender, RoutedEventArgs e)
+        {
+            if(SRPepper.IsChecked==true)
+            {
+                addons();
+
+            }
+            else
+            {
+                removeaddons();
+
+            }
+        }
+
+        private void Mashroom_Checked(object sender, RoutedEventArgs e)
+        {
+            if (mashroom.IsChecked == true)
+            {
+                addons();
+
+            }
+            else
+            {
+                removeaddons();
+
+            }
+        }
+
+        private void RedOnion_Checked(object sender, RoutedEventArgs e)
+        {
+            if (RedOnion.IsChecked == true)
+            {
+                addons();
+
+            }
+            else
+            {
+                removeaddons();
+
+            }
+        }
+
+        private void Pepproni_Checked(object sender, RoutedEventArgs e)
+        {
+            if (pepproni.IsChecked == true)
+            {
+                addons();
+
+            }
+            else
+            {
+                removeaddons();
+
+            }
+        }
+
+        private void Tomato_Checked(object sender, RoutedEventArgs e)
+        {
+            if (tomato.IsChecked == true)
+            {
+                addons();
+
+            }
+            else
+            {
+                removeaddons();
+
+            }
+        }
+
+        private void HotP_Checked(object sender, RoutedEventArgs e)
+        {
+            if (hotP.IsChecked == true)
+            {
+                addons();
+
+            }
+            else
+            {
+                removeaddons();
+
+            }
+        }
+
+        private void Checkout_Click(object sender, RoutedEventArgs e)
+        {
+            Reciept R = new Reciept();
+            R.Show();
+            this.Hide();
+
+        }
+    }
+}
+    
+
+
